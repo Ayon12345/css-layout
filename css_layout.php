efabd872b59dffa8d8ddc12e074b78be6fe408d2
Md. Afsar Mahmud <66775038+Ayon12345@users.noreply.github.com>
2021-04-05 16:56:46 +0600

    layout added using css

---
+++ css_layout.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Welcome to my page</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<style>
+/* Style the body */
+body {
+  font-family: Arial;
+  margin: 0;
+}
+
+
+.header {
+  padding: 30px;
+  text-align: center;
+  background:#E4287C;
+  outline-style: solid;
+  outline-color: rgb(0, 0, 0);
+  color: black;
+  font-size: 30px;
+} 
+
+
+.footer {
+  position: fixed;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  background: #54C571;
+  outline-style: solid;
+  outline-color: rgb(0, 0, 0);
+  color: black;
+  font-size: 30px;
+  text-align: center;
+}
+
+
+/* Page Content */
+.content {
+    padding:165px;
+    position: relative;
+    right: -330px; 
+    width: 70%;
+    background-color: lightsalmon;
+  outline-style: solid;
+  outline-color: rgb(0, 0, 0);
+  color: black;
+  font-size: 30px;
+  text-align: center
+    
+ }
+
+
+
+    ul {
+  list-style-type: none;
+  margin: -20;
+  padding: 0;
+  position: relative;
+    top:  -650px; 
+  width: 60px;
+  color: black;
+  font-size: 30px;
+  outline-style: solid;
+  
+} 
+
+li a {
+  display: block;
+  block-size: 170px;
+  width: 550%;
+  background-color: #6B8E23;
+  outline-color: rgb(0, 0, 0);
+}
+
+
+</style>
+</head>
+<body>
+
+<div class="header">
+  <h1>Welcome to my page!!!</h1>
+  
+</div>
+
+<div class="footer">
+  <p>About us:         Contact: 01***-*****        Email: abc@gmail.com </p>
+</div>
+
+ 
+
+
+<div class="content">
+  <p>My first web page layout usingg css </p>
+</div>
+
+<ul>
+  <li><a href="http://localhost/finalworks/css_layout.php">Home</a></li>
+  <li><a href="https://www.prothomalo.com/">News</a></li>
+  <li><a href="https://www.linkedin.com/in/afsar-ayon-5a350a1a0/">Contact</a></li>
+  <li><a href="https://www.facebook.com/afsarmahmud.ayon.10/">About</a></li>
+</ul>
+
+
+
+
+</body>
+</html>
